277efaaaa0239f25df18f979af67ee3e4a378e39
MikeLowrie <ericmlowrie@gmail.com>
2024-04-16 16:27:22 -0400

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import SplitPane from 'react-split-pane';
 import './App.css';
 
+//https://www.dhiwise.com/post/getting-started-with-react-split-pane-a-step-by-step-tutorial
 function App() {
+  /*
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SplitPane split="vertical" minSize={50} maxSize={1000} defaultSize={200}>
+        <div style={{ background: "#ffa" }}>First</div>
+        <SplitPane split="horizontal" minSize={200} defaultSize={200}>
+          <div style={{ background: "#eee" }}>Second.................</div>
+          <div style={{ background: "#eae" }}>Third</div>
+        </SplitPane>
+      </SplitPane>
+    </div>
+  );
+  */
+  return (
+    <div className="App">
+      <SplitPane split="vertical">
+        <SplitPane split="horizontal" minSize={50} maxSize={1000} defaultSize={200}>
+          <SplitPane split="horizontal">
+            <div>Game one</div>
+            <SplitPane split="horizontal">
+              <div>Game two</div>
+              <div>Game three</div>
+            </SplitPane>
+          </SplitPane>
+        </SplitPane>
+        <SplitPane split="vertical">
+          <SplitPane split="horizontal">
+            <div>Time one</div>
+            <SplitPane split="horizontal">
+              <div>Time two</div>
+              <div>Time three</div>
+            </SplitPane>
+          </SplitPane>
+        </SplitPane>
+      </SplitPane>
     </div>
   );
 }
